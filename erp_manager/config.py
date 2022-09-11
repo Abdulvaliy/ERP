1f2ff0365ba97b2e2e941608e72dd838efcf241d
Abdulvaliy <azokhidov@students.wiut.uz>
2022-09-11 16:19:20 +0500

    Bismillah

--- erp_manager/config.py
+++ erp_manager/config.py
@@ -3,7 +3,7 @@ DEBUG = True
 
 
 class Config:
-    SECRET_KEY = "MyTopSecretKey"  # os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')  # "MyTopSecretKey"  # os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///erp.db'  # os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_PERMANENT = False
